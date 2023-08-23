2ff11375b7545e151153c63e03389d0b2c0ea85b
jeremy reiner <reiner_jeremy@yahoo.com>
2023-08-22 20:37:42 -0500

    Add project files.

---
+++ ArraysAndStrings/MergeArray.cs
@@ -0,0 +1,28 @@
+﻿namespace demo.ArraysAndStrings
+{
+    /// <summary>
+    /// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+    /// and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+    /// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+    /// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    /// Output: [1,2,2,3,5,6]
+    /// </summary>
+    public class MergeArray
+    {
+        int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+
+        int m = 3;
+
+        int[] nums2 = new int[] { 2, 5, 6 };
+
+        int n = 3;
+
+        public MergeArray()
+        {
+            for (var i = 0; i < n; i++)
+                nums1[m + i] = nums2[i];
+
+            Array.Sort(nums1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using demo.ArraysAndStrings;
+
+Console.WriteLine("Hello, World!");
+
+var mergeArray = new MergeArray();

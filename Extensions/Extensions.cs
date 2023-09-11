f17e8636003901e7674ef5be9013dea73e86c656
jeremy reiner <reiner_jeremy@yahoo.com>
2023-09-11 13:41:53 -0500

    Compare Version Solution added

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Extensions
+{
+    public static class Extensions
+    {
+        public static Version ValidVersion(this string version)
+        {
+            if (Version.TryParse(version, out Version v1))
+            {
+                var build = v1.Build != -1
+                    ? v1.Build
+                    : 0;
+                var major = v1.Major != -1
+                    ? v1.Major
+                    : 0;
+                var minor = v1.Minor != -1
+                    ? v1.Minor
+                    : 0;
+                var revision = v1.Revision != -1
+                    ? v1.Revision
+                    : 0;
+
+                return new Version(major, minor, build, revision);
+            }
+
+            if (int.TryParse(version, out var intVersion1))
+            {
+                return new Version(intVersion1, 0, 0, 0);
+            }
+
+            Console.WriteLine($"Invalid version string: {version}");
+
+            var versions = version.Split('.').ToList();
+
+            return ValidVersion(versions.InvalidVersion());
+        }
+
+        private static string InvalidVersion(this List<string> versions)
+        {
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < versions.Count; i++)
+            {
+                if (i > 2)
+                {
+                    if (versions[i] == "0")
+                        continue;
+
+                    //Last instance of invalid can append at the end with no period
+                    builder.Append($"{versions[i]}");
+                }
+                else
+                    builder.Append($"{versions[i]}.");
+            }
+
+            var v = builder.ToString();
+
+            if (v.LastOrDefault() == '.')
+                builder.Append("0");
+
+            return builder.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,4 +6,13 @@ Console.WriteLine("Hello, World!");
 
 var solution = new Solution();
 
-solution.Rotate();
+var result = solution.VersionCompare("2", "2.1.90.12");
+
+var status = result switch
+{
+    < 0 => "less than",
+    > 0 => "greater than",
+    _ => "equal to"
+};
+
+Console.WriteLine($"Version 1 is {status} Version 2");
